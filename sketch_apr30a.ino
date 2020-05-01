6ed7aa62ab66a26b5dc2d438c8039849b0dd302d
LeroyAndrade <30168@ma-web.nl>
2020-05-01 02:55:53 +0200

    Eerste commit
    Eerste push

---
+++ sketch_apr30a.ino
@@ -0,0 +1,215 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  /* put your main code here, to run repeatedly:
+  Todo: geluid controller aanschaffen zodat Dits & Das hoorbaar zijn inplaats van visueel en tempo kan worden verhoogd
+  
+  DITS:
+    digitalWrite(LED_BUILTIN, HIGH); // Ard inschakelen: door voltagie niveau te doen stijgen naar HIGH
+    delay(225);
+    digitalWrite(LED_BUILTIN, LOW);  // Ard uitschakelen: door voltagie niveau te doen verlagen naar LOW -> nog steeds aan 
+    delay(225);                     // Wacht 225 milisecondes 
+ 
+  End block:
+    delay(1225);                     // Wacht 1.2 secondes
+    
+  DAWS:
+     digitalWrite(LED_BUILTIN, HIGH); // Ard inschakelen: door voltagie niveau te doen stijgen naar HIGH
+    delay(450);                      // Wacht 225 milisecondes
+    digitalWrite(LED_BUILTIN, LOW);  // Ard uitschakelen: door voltagie niveau te doen verlagen naar LOW -> nog steeds aan 
+    delay(450);
+
+  End block:
+    delay(1225);
+
+  */
+delay(7000);  
+
+
+delay(1225); 
+  
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(225);
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(225);                     
+
+    digitalWrite(LED_BUILTIN, HIGH); 
+    delay(450);                      
+    digitalWrite(LED_BUILTIN, LOW); 
+    delay(450);
+
+
+delay(1225);  
+    digitalWrite(LED_BUILTIN, HIGH); 
+    delay(450);                     
+    digitalWrite(LED_BUILTIN, LOW);  
+    delay(450);
+
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(225);
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(225); 
+
+
+
+delay(1225);  
+    digitalWrite(LED_BUILTIN, HIGH); 
+    delay(450);                      
+    digitalWrite(LED_BUILTIN, LOW);  
+    delay(450);
+
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(225);
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(225);                    
+  
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(225);
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(225);                     
+
+
+delay(1225);  
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(225);
+  digitalWrite(LED_BUILTIN, LOW); 
+  delay(225); 
+
+    digitalWrite(LED_BUILTIN, HIGH); 
+    delay(450);                      
+    digitalWrite(LED_BUILTIN, LOW); 
+    delay(450);
+
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(225);
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(225);                     
+
+
+delay(1225);  
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(225);
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(225); 
+
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(450);                      
+    digitalWrite(LED_BUILTIN, LOW);  
+    delay(450);
+
+
+delay(1225);  
+    digitalWrite(LED_BUILTIN, HIGH); 
+    delay(450);                     
+    digitalWrite(LED_BUILTIN, LOW); 
+    delay(450);
+
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(225);
+  digitalWrite(LED_BUILTIN, LOW); 
+  delay(225);     
+                  
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(225);
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(225);                     
+
+
+delay(1225);  
+    digitalWrite(LED_BUILTIN, HIGH); 
+    delay(450);                     
+    digitalWrite(LED_BUILTIN, LOW);   
+    delay(450);
+
+
+delay(1225);  
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(225);
+  digitalWrite(LED_BUILTIN, LOW);   
+  delay(225);                     
+  
+    digitalWrite(LED_BUILTIN, HIGH); 
+    delay(450);                      
+    digitalWrite(LED_BUILTIN, LOW);   
+    delay(450);
+
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(225);
+  digitalWrite(LED_BUILTIN, LOW);   
+  delay(225);                    
+
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(225);
+  digitalWrite(LED_BUILTIN, LOW);   
+  delay(225);                    
+
+
+delay(1225);                     
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(225);
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(225);                     
+
+
+delay(1225);                     
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(225);
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(225);                     
+
+    digitalWrite(LED_BUILTIN, HIGH); 
+    delay(450);                      
+    digitalWrite(LED_BUILTIN, LOW);  
+    delay(450);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(225);
+  digitalWrite(LED_BUILTIN, LOW); 
+  delay(225); 
+
+  
+delay(1225);  
+
+    digitalWrite(LED_BUILTIN, HIGH); 
+    delay(450);                    
+    digitalWrite(LED_BUILTIN, LOW);  
+    delay(450);
+       
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(450);                    
+    digitalWrite(LED_BUILTIN, LOW);  
+    delay(450);
+    
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(450);                    
+    digitalWrite(LED_BUILTIN, LOW);  
+    delay(450);
+
+
+delay(1225);  
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(450);                     
+    digitalWrite(LED_BUILTIN, LOW);  
+    delay(450);
+  
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(225);
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(225); 
+
+    digitalWrite(LED_BUILTIN, HIGH); 
+    delay(450);                     
+    digitalWrite(LED_BUILTIN, LOW); 
+    delay(450);
+
+    digitalWrite(LED_BUILTIN, HIGH); 
+    delay(450);                      
+    digitalWrite(LED_BUILTIN, LOW);  
+    delay(450);
+}
+/* Dit vind ik geweldig:
+https://www.youtube.com/watch?v=Li8Hiwbc664
+*/ 
